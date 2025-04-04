aa50a2ca187dcd941ac656ba6dbb079b03868840
Hadar-Sabag <hadarsabag5@gmail.com>
2025-04-04 11:03:05 +0300

    Mobile

--- js/main.js
+++ js/main.js
@@ -4,3 +4,7 @@ function onSwichPage() {
     document.querySelector('.editor').classList.add('hidden')
     document.querySelector('.gallery').classList.remove('hidden')
 }
+
+function toggleMenu() {
+    document.body.classList.toggle("menu-open")
+}
